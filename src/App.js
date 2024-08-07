2a4b657ed934f16c1fe9ac91482937e3ef66b5ab
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-08-07 20:15:37 +0600

    gh-pages

--- src/App.js
+++ src/App.js
@@ -15,18 +15,18 @@ const App = () => {
                 <div>
                     <Navbar style={{ backgroundColor: 'rgb(83, 91, 242)' }} variant="dark" expand="lg">
                         <Container>
-                            <Navbar.Brand as={Link} to="/">Country Info</Navbar.Brand>
+                            <Navbar.Brand as={Link} to="/countries-app">Country Info</Navbar.Brand>
                             <Navbar.Toggle aria-controls="basic-navbar-nav" />
                             <Navbar.Collapse id="basic-navbar-nav">
                                 <Nav className="me-auto">
-                                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                                    <Nav.Link as={Link} to="/countries-app">Home</Nav.Link>
                                 </Nav>
                             </Navbar.Collapse>
                         </Container>
                     </Navbar>
                     <div className="container mt-4">
                         <Routes>
-                            <Route path="/" element={<CountryList />} />
+                            <Route path="/countries-app" element={<CountryList />} />
                             <Route path="/country/:cca3" element={<CountryDetails />} />
                         </Routes>
                     </div>
